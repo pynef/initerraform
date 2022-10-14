68e892440455f7f6dc848d1fe3fafbc760b06fc5
Nefi Arroyo <nefinef@gmail.com>
2022-10-13 20:13:25 -0500

    changing keypair

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "webserver" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  key_name      = "secops_instances_test"
+  key_name      = "secops_instances"
 
   associate_public_ip_address = true
 
