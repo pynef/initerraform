4d05917e8fabf9c87e835952d5220aac59ffe71d
Nefi Arroyo <nefinef@gmail.com>
2022-10-13 19:47:08 -0500

    inicio

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "rimac_inno"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "webserver" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = "secops_instances_test"
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "SecOps"
+  }
+
+}
+
+output "public_ip" {
+  value = aws_instance.webserver.public_ip
+}
